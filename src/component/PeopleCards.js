b33ce5e3af161e5a56fca835d5985b94e8fa4674
min <mmbliv@gmail.com>
2023-06-18 11:48:02 -0500

    people card space wider

--- src/component/PeopleCards.js
+++ src/component/PeopleCards.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { SubtitleIcon } from "./SubtitleIcon"
-import { SubtitleIconSvg } from "./SubtitleIconSvg"
-import GraduatesCards from "./GraduatesCards"
-import PiCards from "./PiCards"
-import PostdocsCards from "./PostdocsCards"
-import UndergraduatesCards from "./UndergraduatesCards"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { SubtitleIcon } from "./SubtitleIcon";
+import { SubtitleIconSvg } from "./SubtitleIconSvg";
+import GraduatesCards from "./GraduatesCards";
+import PiCards from "./PiCards";
+import PostdocsCards from "./PostdocsCards";
+import UndergraduatesCards from "./UndergraduatesCards";
 const query = graphql`
   {
     file(name: { eq: "people" }) {
@@ -16,10 +16,10 @@ const query = graphql`
       publicURL
     }
   }
-`
+`;
 
 const PeopleCards = () => {
-  const data = useStaticQuery(query)
+  const data = useStaticQuery(query);
   return (
     <div id="people">
       <>
@@ -32,11 +32,13 @@ const PeopleCards = () => {
           )}
         </div>
       </>
-      <PiCards />
-      <PostdocsCards />
-      <GraduatesCards />
-      <UndergraduatesCards />
+      <div className="flex flex-col gap-8">
+        <PiCards />
+        <PostdocsCards />
+        <GraduatesCards />
+        <UndergraduatesCards />
+      </div>
     </div>
-  )
-}
-export default PeopleCards
+  );
+};
+export default PeopleCards;
